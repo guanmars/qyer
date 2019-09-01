005a100f6916dfa7077afed980c627fa9c4b01b6
King <408385413@qq.com>
2019-09-01 13:47:46 +0800

    最终

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 //引入express模块
-import express, { static } from "express";
+const express = require("express");
 //引入body-parser模块
-import { urlencoded } from "body-parser";
+const parser = require("body-parser");
 //引入user路由器模块
-import userRouter from "./router/user.js";
+const userRouter = require("./router/user.js");
 //创建web服务器
-var app=express();
+var app = express();
 //监听端口
 app.listen(8080);
 //把静态资源托管到public目录下
 // app.use(express.static("public"));
-app.use(static("public"));
-app.use(urlencoded({
-	extended:false
+app.use(express.static("public"));
+app.use(parser.urlencoded({
+	extended: false
 }));
 //使用路由器，挂载到/user下
-app.use("/user",userRouter);
-//使用路由器，挂载到/demo下
-app.use("/demo",demoRouter);
-app.use("/pro",proRouter);
+app.use("/user", userRouter);
